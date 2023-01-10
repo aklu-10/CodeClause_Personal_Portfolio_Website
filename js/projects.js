cc1fc3d2eca1dd9f7f0475ec3cb5414783c73823
Akash <you@example.com>
2023-01-10 16:33:50 +0530

    Portfolio Website - CodeClause Project

---
+++ js/projects.js
@@ -0,0 +1,100 @@
+let projects=
+[
+    {
+        name:"Swagshop",
+        tools:"HTML, CSS & JS",
+        image:"/images/projects/websites/swagshop.png",
+        url:"https://aklu-10.github.io/SwagShop-WebDesign/",
+        type:"websites"
+    },
+    {
+        name:"Webder",
+        tools:"HTML, CSS & JS",
+        image:"/images/projects/websites/webder.png",
+        url:"https://aklu-10.github.io/Webder-WebDesign/",
+        type:"websites"
+    },
+    {
+        name:"Rectarc",
+        tools:"HTML, CSS & JS",
+        image:"/images/projects/websites/rectarc.png",
+        url:"https://aklu-10.github.io/Rectarc-WebDesign/",
+        type:"websites"
+    },
+    {
+        name:"Ebuy",
+        tools:"HTML, CSS & Bootstrap",
+        image:"/images/projects/websites/Ebuy.png",
+        url:"https://aklu-10.github.io/Ebuy-WebDesign/",
+        type:"websites"
+    },
+    {
+        name:"Sugary Design",
+        tools:"Adobe Illustrator",
+        image:"/images/projects/illus/Background2.png",
+        url:"/images/projects/illus/Background2.png",
+        type:"illustrations"
+    },
+    {
+        name:"Geometric Design",
+        tools:"Adobe Illustrator",
+        image:"/images/projects/illus/Bird.png",
+        url:"/images/projects/illus/Bird.png",
+        type:"illustrations"
+    },
+    {
+        name:"Desert",
+        tools:"Adobe Illustrator & Silhouette",
+        image:"/images/projects/illus/desert.jpg",
+        url:"/images/projects/illus/desert.jpg",
+        type:"illustrations"
+    },
+    {
+        name:"Wallpaper",
+        tools:"Adobe Illustrator & Silhouette",
+        image:"/images/projects/illus/screensaver.png",
+        url:"/images/projects/illus/screensaver.png",
+        type:"illustrations"
+    },
+    {
+        name:"Scenery",
+        tools:"Adobe Illustrator & Silhouette",
+        image:"/images/projects/illus/wolf_at_night.jpg",
+        url:"/images/projects/illus/wolf_at_night.jpg",
+        type:"illustrations"
+    },
+    {
+        name:"Scenery",
+        tools:"Adobe Illustrator & Silhouette",
+        image:"/images/projects/illus/sunset.jpg",
+        url:"/images/projects/illus/sunset.jpg",
+        type:"illustrations"
+    },
+    {
+        name:"React NoteKeeper",
+        tools:"Reactjs",
+        image:"/images/projects/other/react_notekeeper.png",
+        url:"https://quicknotekeeper.netlify.app/",
+        type:"others"
+    },
+    {
+        name:"React DigitalClock",
+        tools:"Reactjs",
+        image:"/images/projects/other/react_digiclock.png",
+        url:"https://vocal-paprenjak-3e789b.netlify.app/",
+        type:"others"
+
+    },
+    {
+        name:"React CryptoTable",
+        tools:"Reactjs",
+        image:"/images/projects/other/react_cryptotable.png",
+        url:"https://incredible-shortbread-58b6b8.netlify.app/",
+        type:"others"
+    }
+     
+
+    
+]
+
+export default projects;
